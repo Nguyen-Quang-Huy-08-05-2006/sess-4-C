e2eafef8fd22fa9aa6514b474977120f6ba5cdcb
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-25 01:19:34 +0700

    Update btvn7Sess4.cpp

--- btvn7Sess4.cpp
+++ btvn7Sess4.cpp
@@ -4,7 +4,7 @@ int main(){
 	printf("nhap 1 nam");
 	scanf("%4d",&year);
 	
-	if(year%4==0){
+	if(yyyy%4==0&&yyyy!=100||yyyy%400==0){
 		printf("day la nam nhuan");
 	}else{
 		printf("day khong phai nam nhuan");
