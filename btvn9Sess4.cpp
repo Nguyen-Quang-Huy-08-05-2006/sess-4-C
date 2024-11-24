b64ccf76bc186db04be0c011d57bcd5dbb1ba9a8
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-25 01:19:48 +0700

    Update btvn9Sess4.cpp

--- btvn9Sess4.cpp
+++ btvn9Sess4.cpp
@@ -4,13 +4,13 @@ int main(){
 	int mm;
 	int yyyy;
 	printf("nhap vao ngay");
-	scanf("%2d", dd);
+	scanf("%2d", &dd);
 	
 	printf("nhap vao thang");
-	scanf("%2d", mm);
+	scanf("%2d", &mm);
 	
 	printf("nhap vao nam");
-    scanf("%4d", yyyy);
+    scanf("%4d", &yyyy);
     
     if(dd>=1 && dd<= 31 && mm==1 && mm==3 && mm==5 && mm==7 && mm==8 && mm==11 && mm==12){
     	printf("ngay thang nam ho le");
