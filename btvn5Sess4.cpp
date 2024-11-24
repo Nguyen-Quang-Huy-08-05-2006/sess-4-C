b30f4d30af58f1e477f644174d3a328a78e23c65
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-25 01:13:05 +0700

    Update btvn5Sess4.cpp

--- btvn5Sess4.cpp
+++ btvn5Sess4.cpp
@@ -6,10 +6,12 @@ int main(){
 	printf("moi ban nhap vao 3 so nguyen : \n");
 	scanf("%d %d %d", a, b, c);
 	
-	if(b<c && b>a){
-		printf("so %d nam giua %d va %d \n", b, a, c);
-	}else{
-		printf("so %d khong nam giua %d va %d \n", b, a, c);
+	if(c<b && c>a){
+		printf("so %d nam giua %d va %d \n", c, a, b);
+	}else if(c>b && c<a){
+		printf("so %d nam giua %d va %d \n", c, a, b);
+	}else
+		printf("so %d khong nam giua %d va %d \n", c, a, b);
 	}
 	
 	return 0;
